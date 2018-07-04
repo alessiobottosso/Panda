d92aa2254844ea0b0e7b2712047e46ac5c6c352f
botoman <alessio.bottosso@gmail.com>
2018-07-04 11:16:54 +0200

    Fullscreen

--- src/game/main.js
+++ src/game/main.js
@@ -7,10 +7,19 @@ game.addAsset('panda.png');
 
 game.createScene('Main', {
     init: function() {
+        makeFullscreen('canvas');
         var player = new game.Player();
         player.sprite.addTo(this.stage);
     }
 });
+
+function makeFullscreen(id)
+{	var el = document.getElementById(id);	
+    if	(el.requestFullScreen) el.requestFullScreen();	
+    else if	(el.webkitRequestFullScreen) el.webkitRequestFullScreen();	
+    else if	(el.mozRequestFullScreen   ) el.mozRequestFullScreen();
+}
+
 
 game.createClass('Player', {
     init: function() {
