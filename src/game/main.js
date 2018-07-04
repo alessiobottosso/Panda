4f862fafd45089583dcd89b104431547339b5870
Stefano Peranzoni <s.peranzoni@reply.it>
2018-07-04 12:03:01 +0200

    test1

--- src/game/main.js
+++ src/game/main.js
@@ -5,11 +5,23 @@ game.module(
 
 game.addAsset('panda.png');
 
-game.createScene('Main', {
-    init: function() {
-        makeFullscreen('canvas');
+game.createScene('Main', 
+{
+    init: function() 
+    {
         var player = new game.Player();
         player.sprite.addTo(this.stage);
+        
+        var tween = new game.Tween(player.sprite.position);
+        tween.to({ x: 200, y: 200 }, 2000);
+        tween.start();
+
+        game.Timer.add(1000, function() 
+        {
+            makeFullscreen('canvas');
+            tween.pause();
+            var remaining = tween.duration - tween.currentTime;
+        });
     }
 });
 
