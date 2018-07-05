ac2853462b9fad3b30a4e1da0e7c78e0fa62d5eb
botoman <alessio.bottosso@gmail.com>
2018-07-05 09:52:15 +0200

    fixing values.

--- src/game/main.js
+++ src/game/main.js
@@ -18,7 +18,7 @@ game.createScene('Main', {
         this.sprite.x = game.width / 2 - accel.x * 20;
         this.sprite.y = game.height / 2 - accel.y * 20;
 
-        if ((this.sprite.y >= game.height / 2 + 1) || (this.sprite.y < game.height / 2 - 1))
+        if ((this.sprite.y >= game.height / 2 + 10) || (this.sprite.y < game.height / 2 - 10))
         {
             navigator.vibrate(200);
         }
