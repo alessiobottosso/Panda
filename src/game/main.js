78d33d4cc8cc49593a0778eadeaff32df4816e63
botoman <alessio.bottosso@gmail.com>
2018-07-05 10:13:45 +0200

    Adding text.

--- src/game/main.js
+++ src/game/main.js
@@ -10,6 +10,9 @@ game.createScene('Main', {
         this.sprite = new game.Sprite('panda.png');
         this.sprite.anchorCenter();
         this.sprite.addTo(this.stage);
+
+        var text = new game.Text('Hello Panda');
+        text.addTo(this.stage);
     },
     
     update: function() {
@@ -17,6 +20,8 @@ game.createScene('Main', {
         var accel = game.input.motion.accelerationIncludingGravity;
         this.sprite.x = game.width / 2 - accel.x * 20;
         this.sprite.y = game.height / 2 - accel.y * 20;
+
+        text.setText ("Accel y: " + accel.y);
 
         if (accel.y >= 15 || accel.y <= -15)
         {
