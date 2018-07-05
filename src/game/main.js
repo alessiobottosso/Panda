c7b9229b62ce2c93ed77a997db842b086300ddc9
botoman <alessio.bottosso@gmail.com>
2018-07-05 11:54:01 +0200

    Trying to plot a graph.

--- src/game/main.js
+++ src/game/main.js
@@ -6,44 +6,70 @@ game.module(
 game.addAsset('panda.png');
 
 game.createScene('Main', {
-    init: function() {
+    init: function() 
+    {
+        this.container = new game.Container();
+        this.container.addTo(this.stage);
+        
         this.sprite = new game.Sprite('panda.png');
         this.sprite.anchorCenter();
-        this.sprite.addTo(this.stage);
+        //this.sprite.addTo(this.stage);
+        
+        this.sprite.x = game.width / 2;
+        this.sprite.y = game.height / 2;
+        
+        this.sprite.addTo(this.container);
 
         myText = new game.SystemText('Hello Panda');
         myText.addTo(this.stage);
 
         maxY = 0;
         minY = 0;
+        
+        var camera = new game.Camera(this.sprite);
+        camera.addTo(this.container);
     },
     
     update: function() 
     {
-        if (!game.input.motion) return;
-        var accel = game.input.motion.accelerationIncludingGravity;
-        this.sprite.x = game.width / 2 - accel.x * 20;
-        this.sprite.y = game.height / 2 - accel.y * 20;
-
-        if (accel.y > maxY)
+        this.sprite.x += 200 * game.delta;
+        
+        var grap = new game.Graphics();
+        grap.drawCircle(5, 5, 5);
+        grap.x = this.sprite.x;
+        grap.y = this.sprite.y;
+        grap.addTo(this.container);
+        
+        if (!game.input.motion) 
         {
-            maxY = accel.y;
-        }
-        if (accel.y < minY)
-        {
-            minY = accel.y;
-        }
-
-        //myText.text = "Max Y: " + maxY + " - Min Y: " + minY;
-
-        if (accel.y >= 15 || accel.y <= -15)
-        {
-            myText.text = "VIBRATE";
-            navigator.vibrate(5);
+            return;
         }
         else
         {
-            myText.text = "";
+            var accel = game.input.motion.accelerationIncludingGravity;
+            //this.sprite.x = game.width / 2 - accel.x * 20;
+            this.sprite.y = game.height / 2 - accel.y * 20;
+    
+            if (accel.y > maxY)
+            {
+                maxY = accel.y;
+            }
+            if (accel.y < minY)
+            {
+                minY = accel.y;
+            }
+    
+            //myText.text = "Max Y: " + maxY + " - Min Y: " + minY;
+    
+            if (accel.y >= 15 || accel.y <= -15)
+            {
+                myText.text = "VIBRATE";
+                navigator.vibrate(5);
+            }
+            else
+            {
+                myText.text = "";
+            }
         }
     }
 });
