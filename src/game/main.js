431be37ddeaf194e14bd271fbd92d651219d91fd
botoman <alessio.bottosso@gmail.com>
2018-07-10 17:01:22 +0200

    Playing with physics.

--- src/game/main.js
+++ src/game/main.js
@@ -13,12 +13,29 @@ game.addAsset('spineboy.json');
 game.createScene('Main', {
     init: function() 
     {
+        this.world = new game.Physics();
+        
         this.player = new game.Spine('spineboy.json');
         this.player.position.x = game.width / 2;
         this.player.position.y = game.height / 2;
         this.player.scale.x = 0.35;
         this.player.scale.y = 0.35;
         this.player.play('walk', true);
+        
+        this.body = new game.Body();
+        this.body.position.set(
+            this.player.position.x, 
+            this.player.position.y );
+        
+        this.body.mass = 1;
+        
+        var shape = new game.Rectangle();
+        shape.width = this.player.width;
+        shape.height = this.player.height;
+        this.body.addShape(shape);
+        this.body.addTo(game.scene.world);
+        
+        
         this.player.addTo(this.stage);
         
         this.player.isWalking = true;
@@ -48,6 +65,38 @@ game.createScene('Main', {
     
     update: function() 
     {
+        this.player.position.copy(this.body.position);
+        this.player.position.y += 100;
+        
+        var width  = this.body.shape.width / 2;
+        var height = this.body.shape.height / 2;	
+        
+        // Top		
+        if(this.body.position.y - height < 0) {			
+            this.body.position.y -= this.body.position.y - height;			
+            this.body.velocity.y *= -0.75;	
+        }       	
+        
+        // Bottom
+        if(this.body.position.y + height > game.system.height) 
+        {			
+            this.body.position.y += game.system.height - this.body.position.y - height;			
+            this.body.velocity.y *= -0.75;		
+        }
+        
+        // right		
+        if(this.body.position.x + width > game.system.width) 
+        {			
+            this.body.position.x += game.system.width - this.body.position.x - width;			
+            this.body.velocity.x *= -0.75;		
+        }
+        
+        if(this.body.position.x - width < 0) 
+        {			
+	        this.body.position.x -= this.body.position.x - width;			
+	        this.body.velocity.x *= -0.75;			
+        }	
+        
         var oldx = this.player.x;
         var oldy = this.player.y;
         
@@ -73,6 +122,14 @@ game.createScene('Main', {
                     this.player.isRunning = true;
                     this.player.play('run', true);
                 }
+                if (this.body.velocity.y >= 0)
+                {
+                    this.body.velocity.y += 50;	
+                }
+                else
+                {
+                    this.body.velocity.y -= 50;	
+                }
             }
             else if (game.keyboard.down('DOWN'))
             {
@@ -83,7 +140,52 @@ game.createScene('Main', {
                     this.player.isRunning = true;
                     this.player.play('run', true);
                 }
+                if (this.body.velocity.y >= 0)
+                {
+                    this.body.velocity.y += 50;	
+                }
+                else
+                {
+                    this.body.velocity.y -= 50;	
+                }
             }
+            else if (game.keyboard.down('RIGHT'))
+            {
+                if (this.player.isWalking === true)
+                {
+                    //this.sprite.y += 200 * game.delta;
+                    this.player.isWalking = false;
+                    this.player.isRunning = true;
+                    this.player.play('run', true);
+                }
+                if (this.body.velocity.x >= 0)
+                {
+                    this.body.velocity.x += 50;	
+                }
+                else
+                {
+                    this.body.velocity.x -= 50;	
+                }
+            }
+            else if (game.keyboard.down('LEFT'))
+            {
+                if (this.player.isWalking === true)
+                {
+                    //this.sprite.y += 200 * game.delta;
+                    this.player.isWalking = false;
+                    this.player.isRunning = true;
+                    this.player.play('run', true);
+                }
+                if (this.body.velocity.x >= 0)
+                {
+                    this.body.velocity.x += 50;	
+                }
+                else
+                {
+                    this.body.velocity.x -= 50;	
+                }
+            }
+            
             else
             {
                 //this.player.play('walk', true);
@@ -98,9 +200,9 @@ game.createScene('Main', {
         else
         {
             var accel = game.input.motion.accelerationIncludingGravity;
-            this.player.x = game.width / 2 - accel.x * 20;
-            this.player.y = game.height / 2 - accel.y * 20;
-    
+            //this.player.x = game.width / 2 - accel.x * 20;
+            //this.player.y = game.height / 2 - accel.y * 20;
+            /*
             if (accel.y > maxY)
             {
                 maxY = accel.y;
@@ -109,14 +211,39 @@ game.createScene('Main', {
             {
                 minY = accel.y;
             }
+            */
+            
+            if (accel.x >= 10)
+            {
+                if (this.body.velocity.y >= 0)
+                {
+                    this.body.velocity.x += 20;	
+                }
+                else
+                {
+                    this.body.velocity.x -= 20;	
+                }
+            }
+            
+            if (accel.y >= 10)
+            {
+                if (this.body.velocity.y >= 0)
+                {
+                    this.body.velocity.y += 20;	
+                }
+                else
+                {
+                    this.body.velocity.y -= 20;	
+                }
+            }
     
             //myText.text = "Max Y: " + maxY + " - Min Y: " + minY;
             //myText.text = game.debug.fps;
 
-            if (accel.y >= 10 || accel.y <= -10)
+            if (accel.y >= 15 || accel.y <= -15)
             {
                 //myText.text = "VIBRATE";
-                navigator.vibrate(5);
+                //navigator.vibrate(1);
                 /*
                 if (this.player.isWalking === true)
                 {
