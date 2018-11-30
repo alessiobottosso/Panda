fce8ae38a58a3a2dda455534133db5377b303492
Stefano Peranzoni <s.peranzoni@reply.it>
2018-11-30 18:02:23 +0100

    Update main.js

--- j/coloringText/src/game/main.js
+++ j/coloringText/src/game/main.js
@@ -8,6 +8,11 @@ game.addAsset('minecraft.fnt');
 game.createScene('Main', {
     init: function() 
     {
+        if(game.auto===undefined)
+        {
+            game.auto=false;
+        }
+        
     var text=Math.random()*1000;
     text=~~(text)
     var etext = new game.Text(text);
@@ -18,21 +23,38 @@ game.createScene('Main', {
 		etext._cachedSprite.tint = "#668899";
 		etext._cachedSprite.tintAlpha = 1; 	
 	this.scenetext = etext
+            
     },
 
     update: function() {
-        
+        if(game.auto)
+        {
+            game.scene.doChangeScene();
+        }
+    },
+    doChangeScene:function()
+    {
+            // var canvasID =this.scenetext._cachedSprite.texture.baseTexture._id;
+            // var canvas = document.getElementById(canvasID); //because we are looping //each location has its own canvas ID
+            // var context = canvas.getContext('2d');
+
+            var canvas =this.scenetext._cachedSprite.texture.baseTexture.source;
+            delete canvas
+            this.scenetext._cachedSprite.texture.remove();
+            this.scenetext.remove();
+            game.system.setScene("Main");
+
     },
     keydown: function(key) 
     {
         if (key === 'SPACE') 
         {
-            delete this.scenetext._cachedSprite.texture.baseTexture;
-            this.scenetext._cachedSprite.texture.remove();
-            this.scenetext.remove();
-            game.system.setScene("Main");
+            game.scene.doChangeScene();
         }
-        
+        if (key === 'A') 
+        {
+            game.auto=true;
+        }
     }
 
 });
