735f9ad4e2fcf95d05fe948d91af96a5d8d5461a
Stefano Peranzoni <s.peranzoni@reply.it>
2018-12-03 11:11:32 +0100

    Update main.js

--- j/coloringText/src/game/main.js
+++ j/coloringText/src/game/main.js
@@ -12,20 +12,28 @@ game.createScene('Main', {
         {
             game.auto=false;
         }
+    for(var j=0;j<100;++j)
+    {
+        var x=j%10;
+        var y=~~(j/10);
+        this.createText(70*x,100*y);
         
-    var text=Math.random()*1000;
+    }
+            
+    },
+    createText:function(x,y){
+    var text=100+Math.random()*900;
     text=~~(text)
     var etext = new game.Text(text);
     etext.addTo(this.stage)
-    
-        
-        etext.cache = true;
-		etext._cachedSprite.tint = "#668899";
-		etext._cachedSprite.tintAlpha = 1; 	
-	this.scenetext = etext
-            
+    etext.cache = true;
+	etext._cachedSprite.tint = "#668899";
+	etext._cachedSprite.tintAlpha = 1; 	
+	etext.width=50;
+	etext.x=x;
+	etext.y=y;
+    return etext;        
     },
-
     update: function() {
         if(game.auto)
         {
@@ -38,10 +46,10 @@ game.createScene('Main', {
             // var canvas = document.getElementById(canvasID); //because we are looping //each location has its own canvas ID
             // var context = canvas.getContext('2d');
 
-            var canvas =this.scenetext._cachedSprite.texture.baseTexture.source;
-            delete canvas
-            this.scenetext._cachedSprite.texture.remove();
-            this.scenetext.remove();
+            // var canvas =this.scenetext._cachedSprite.texture.baseTexture.source;
+            // delete canvas
+            // this.scenetext._cachedSprite.texture.remove();
+            // this.scenetext.remove();
             game.system.setScene("Main");
 
     },
