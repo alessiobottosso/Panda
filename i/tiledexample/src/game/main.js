776db1b8fdbff6ce838fca6ce8e987e9a9c0343c
Stefano Peranzoni <s.peranzoni@reply.it>
2018-12-05 17:19:14 +0100

    update

--- i/tiledexample/src/game/main.js
+++ i/tiledexample/src/game/main.js
@@ -1,6 +1,8 @@
 game.module(
     'game.main'
 )
+.require(
+    'plugin.pixi')
 .body(function() {
 
 game.addAsset('sprite.png');
@@ -9,7 +11,18 @@ game.createScene('Main', {
     init: function() {
     	this.sprite = new game.TilingSprite('sprite.png', game.width, game.height);
     	this.sprite.addTo(this.stage);
-
+        var filter = new game.filters.CRTFilter();
+        filter.vignetting=0.3
+        filter.curvature=2;
+        var filter1 = new game.filters.RGBSplitFilter();
+        filter1.blue[0]=0;
+        filter1.blue[1]=0;
+        filter1.green[0]=0;
+        filter1.green[1]=2;
+        filter1.red[0]=-2;
+        filter1.red[1]=0;
+        
+        this.sprite.filters = [filter1,filter];
     	this.sprite2 = new game.Sprite('sprite.png');
     	this.sprite2.anchorCenter();
     	this.sprite2.x = game.width/2
